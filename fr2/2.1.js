3643f2759b38e18b8e62bc812f5557b2d84e4b1b
takagotch <dyaccb@gmail.com>
2018-01-30 10:47:21 +0900

    .each(source, function(idx, region){ var input = ().attr().attr(); if(region){ ().prop(); })

--- fr2/2.1.js
+++ fr2/2.1.js
@@ -1,3 +1,38 @@
+$.grep(
+  source,
+  function(obj){ return obj.show; }
+)
+
+$.map(
+  source,
+  function(obj){ return { data: obj.data, color: obj.color }; }
+)
+
+$.map(
+  $.grep(
+    source,
+    function(obj) { return obj.show; }
+  ),
+  function(obj){ return { dataL obj.data, color: obj.color }; }
+)
+
+$.each(source, function(idx, region){
+  var input = $("<input>").attr("type", "checkbox").attr("id", "chk-"+idx);
+  if(region.show){
+    $(input).prop("checked", true);
+  }
+  var span = $("<span>").css({
+    "background-color": region.color,
+    "display": "inline-block",
+    "height": "0.9em",
+    "width": "0.9em",
+    "margin-right": "0.25em"
+  });
+  var label = $("<label>").append(input).append(span).append(region.name);
+  $("#controls").append(label);
+});
+
+
 
 $("input[id^='chk-'").click(function(ev){
   //...
